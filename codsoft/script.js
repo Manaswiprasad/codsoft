423a04de95e5ba65325fb6a93e43a217c3a688db
Manaswiprasad <manaswianchoori@gmail.com>
2025-03-31 18:03:49 +0530

    Add files via upload

---
+++ codsoft/script.js
@@ -0,0 +1,9 @@
+function showQuizCreation() {
+    document.getElementById('quizCreation').style.display = 'block';
+    document.getElementById('quizTaking').style.display = 'none';
+}
+
+function showQuizTaking() {
+    document.getElementById('quizTaking').style.display = 'block';
+    document.getElementById('quizCreation').style.display = 'none';
+}
